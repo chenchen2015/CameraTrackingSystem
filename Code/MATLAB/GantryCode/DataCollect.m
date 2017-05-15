6189a44bc422b20a53ed05337dc993874996e552
Chen Chen <chenchen.bme@gmail.com>
2017-05-15 12:21:23 -0500

    A working version of automated labelled data generator is finished.

--- Code/MATLAB/GantryCode/DataCollect.m
+++ Code/MATLAB/GantryCode/DataCollect.m
@@ -1,27 +1,23 @@
-mex Console_FrameGrabber_v1.cpp XCLIBW64.lib PXIPLW64.lib CaptureLog.cpp;
-g = GantryMove('192.168.1.121');
-g.Connect;
-g.goToPos([-0.7,-0.4,-0.2,0]);
-pause(10);
+%mex Console_FrameGrabber_v1.cpp XCLIBW64.lib PXIPLW64.lib CaptureLog.cpp;
+%g = GantryMove('192.168.1.121');
+sampleNum = 5;
+%gantryPos = zeros(sampleNum,2);
+%g.Connect;
+%g.goToPos([-0.7,-0.4,-0.2,0]);
+%pause(10);
 x = -0.7;
 y = -0.4;
-for i = 1:5
+for i = 1:sampleNum
     x = x + 0.3;
     y = y + 0.15;
-    g.goToPos([x,y,-0.2,0]);
-    pause(20);
-    Console_FrameGrabber_v1();
+    %g.goToPos([x,y,-0.2,0]);
+    %pause(20);
+    %Console_FrameGrabber_v1();
+    gantryPos(i,1) = x;
+    gantryPos(i,2) = y;
 end
-g.goToPos([0.7,-0.4,-0.2,0]);
-pause(10);
-x = 0.7;
-y = -0.4;
-for i = 1:5
-    x = x - 0.3;
-    y = y + 0.15;
-    g.goToPos([x,y,-0.2,0]);
-    pause(20);
-    Console_FrameGrabber_v1();
+%g.goToPos([-0.7,-0.4,0.2,0]);
+%g.Disconnect;
+for i = 1:sampleNum
+    trackLEDs(i, gantryPos(i,:));
 end
-g.goToPos([-0.7,-0.4,0.2,0]);
-g.Disconnect;
---
+++ Code/MATLAB/GantryCode/trackLEDs.m
@@ -0,0 +1,82 @@
+function trackLEDs(dataSetNumber, gantryPosition)
+%% Track IR LEDs and Save Tracked Result
+
+%% File Select
+imgPath = 'Z:\Repos\CameraTrackingSystem-GitHub\branches\Sherwin_branch\Code\MATLAB\GantryCode\Imgs\';
+allFiles = dir(fullfile(imgPath, '*.bmp'));
+imgFileName = {allFiles.name};
+
+%% Process Each Image
+numOfImgs = 3;
+
+% Preallocate Memory
+trackedLED(numOfImgs).LEDs = struct;
+trackedLED(numOfImgs).Centroid = [];
+
+% Image Plot Handles
+figure(dataSetNumber);clf;
+im = imread([imgPath,imgFileName{dataSetNumber}]);
+bw = im2bw(im,graythresh(im));
+hIm = imshow(bw);hold on;
+hLED = plot(0,0,'g.','MarkerSize',24);
+
+for i = 1:numOfImgs
+    start = (dataSetNumber-1)*3;
+    im = imread([imgPath,imgFileName{i+start}]);
+    bw = im2bw(im,graythresh(im));
+    
+    % Do blob detection
+    stats = regionprops('table',bw,'Centroid','ConvexArea');
+    
+    stats = stats(stats.Centroid(:,2) > 500 & stats.Centroid(:,2) < 1700,:);
+    
+    if numel(stats.ConvexArea) == 3
+        trackedLED(i).LEDs = stats;
+        trackedLED(i).Centroid = mean(stats.Centroid);
+        trackedLED(i).Area = mean(stats.ConvexArea);
+        
+        fprintf('LED tracked - norm diff = %.2f\n',...
+            norm(diff(trackedLED(i).LEDs.Centroid(:,2))));
+    elseif numel(stats.ConvexArea) > 3
+        [~,idx] = sort(stats.Centroid(:,2),'descend');
+        
+        if norm(diff(stats(idx(1:3),:).Centroid(:,2))) > 10
+            trackedLED(i).LEDs = stats(idx(1),:);
+            trackedLED(i).Centroid = stats(idx(1),:).Centroid;
+            trackedLED(i).Area = stats(idx(1),:).ConvexArea;
+            fprintf('LED tracked\n');
+        else
+            trackedLED(i).LEDs = stats(idx(1:3),:);
+            trackedLED(i).Centroid = mean(stats(idx(1:3),:).Centroid);
+            trackedLED(i).Area = mean(stats(idx(1:3),:).ConvexArea);
+            
+            fprintf('LED tracked - norm diff = %.2f\n',norm(diff(trackedLED(i).LEDs.Centroid(:,2))));
+        end
+        
+    elseif ~isempty(stats)
+        [~,idx] = sort(stats.Centroid(:,2),'descend');
+        trackedLED(i).LEDs = stats(idx(1),:);
+        trackedLED(i).Centroid = stats(idx(1),:).Centroid;
+        trackedLED(i).Area = stats(idx(1),:).ConvexArea;
+    elseif isempty(stats)
+        trackedLED(i).LEDs = stats;
+        trackedLED(i).Centroid = stats.Centroid;
+        trackedLED(i).Area = stats.ConvexArea;
+    end
+    
+    % Update Plot
+    hIm.CData = bw;
+    hLED.XData = trackedLED(i).LEDs.Centroid(:,1);
+    hLED.YData = trackedLED(i).LEDs.Centroid(:,2);
+    %%pause;
+end
+
+%% Assemble Output Data Structure
+dataSetStr = num2str(dataSetNumber);
+Comment = strcat('DataSet', dataSetStr);
+SampleTime = datestr(now,'mm-dd-yyyy');
+
+% Save Data
+save(Comment,'trackedLED','gantryPosition','SampleTime');
+
+end
